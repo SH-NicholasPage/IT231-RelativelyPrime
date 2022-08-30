7c8f6df79bfab391e11d8dd9202ad343b3e10656
SH-NicholasPage <npage@southhills.edu>
2022-08-30 18:28:32 -0400

    Finished

---
+++ RelativelyPrime/RelativelyPrime/Grader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RelativelyPrime
+{
+    internal class Grader
+    {
+        static int Main(String[] args)
+        {
+            try
+            {
+                if (args.Length > 2)
+                {
+                    return -1;
+                }
+
+                int value1 = int.Parse(args[0]);
+                int value2 = int.Parse(args[1]);
+
+                return (GCD(value1, value2) == 1) ? 1 : 0;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        static int GCD(int a, int b)
+        {
+            // Everything divides 0
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+
+            // base case
+            if (a == b)
+            {
+                return a;
+            }
+
+            // a is greater
+            if (a > b)
+            {
+                return GCD(a - b, b);
+            }
+
+            return GCD(a, b - a);
+        }
+    }
+}
---
+++ RelativelyPrime/RelativelyPrime/Source.cs
@@ -0,0 +1,17 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelativelyPrime
+{
+    internal class Source
+    {
+    }
+}
